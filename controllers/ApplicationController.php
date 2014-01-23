8b606eb5266c04532be85cc2f16ef0916e5e3d3f
unknown <Travis.Pessetto@ES3UT-NB349.es3inc.com>
2014-01-23 16:18:22 -0700

    Fix redirect error messages not showing up

--- controllers/ApplicationController.php
+++ controllers/ApplicationController.php
@@ -5,7 +5,7 @@ class ApplicationController extends Controller
 	public function __construct()
 	{
 		parent::__construct();
-		$this->before_filter("check_login",["except"=>["index"]]);
+		$this->before_filter("check_login",["except"=>["index","sign_in"]]);
 	}
 	
 	public function check_login()
